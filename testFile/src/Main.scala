fb95a203a4ae19151033d24ffae63fdc4a8a675b
Elizabeth Porter <eliporte1@WKMZT97C136B.global.publicisgroupe.net>
2022-09-29 13:46:12 -0500

    change in remote

--- testFile/src/Main.scala
+++ testFile/src/Main.scala
@@ -2,6 +2,6 @@ object Main {
   def main(args: Array[String]): Unit = {
   	println("Hello Elizabeth")
     change in remote 
-    another change in remote
+change in local    another change in remote
   }
 }
